a8f1a04df5af0f57885da5ba157094c3fad0ce4b
Ahmed-ashraf1111 <129650864+Ahmed-ashraf1111@users.noreply.github.com>
2025-09-10 22:06:02 +0300

    get all user& get all posts

---
+++ Backend-Alumni/src/routes/user.route.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+const adminController = require("../controllers/admin.controller");
+const userController = require("../controllers/user.controller");
+
+// GET all users (graduates + staff)
+router.get("/", userController.getAllUsers);
+
+module.exports = router;
