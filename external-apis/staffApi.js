aef46b7021a9ed416d739a180dbca7cf119e319a
Maloka-119 <aishafathy647@gmail.com>
2025-11-09 03:52:44 +0200

    update staff db

--- external-apis/staffApi.js
+++ external-apis/staffApi.js
@@ -5,16 +5,17 @@ app.use(express.json());
 
 // Mock Database
 const staff = [
-  { nationalId: "30003151234577", department: "Computer Science" }, // 2000-03-15
-  { nationalId: "30104161234578", department: "Mathematics" },     // 2001-04-16
-  { nationalId: "30005271234579", department: "Physics" },         // 2000-05-27
-  { nationalId: "30106301234580", department: "Chemistry" },       // 2001-06-30
-  { nationalId: "30007411234581", department: "Biology" },         // 2000-07-04
-  { nationalId: "30108521234582", department: "English" },         // 2001-08-05
-  { nationalId: "30009631234583", department: "History" },         // 2000-09-06
-  { nationalId: "30110741234584", department: "Geography" },       // 2001-10-07
-  { nationalId: "30011821234585", department: "Economics" },       // 2000-11-18
-  { nationalId: "30112911234586", department: "Philosophy" },      // 2001-12-29
+{ nationalId: "30003150102341", department: "Computer Science" }, // 2000-03-15
+{ nationalId: "30104160104562", department: "Mathematics" },     // 2001-04-16
+{ nationalId: "30005270107893", department: "Physics" },         // 2000-05-27
+{ nationalId: "30106300101254", department: "Chemistry" },       // 2001-06-30
+{ nationalId: "30007040105678", department: "Biology" },         // 2000-07-04
+{ nationalId: "30108050108912", department: "English" },         // 2001-08-05
+{ nationalId: "30009060102345", department: "History" },         // 2000-09-06
+{ nationalId: "30110070104561", department: "Geography" },       // 2001-10-07
+{ nationalId: "30011180107892", department: "Economics" },       // 2000-11-18
+{ nationalId: "30112290101253", department: "Philosophy" },      // 2001-12-29
+
 ];
 
 
