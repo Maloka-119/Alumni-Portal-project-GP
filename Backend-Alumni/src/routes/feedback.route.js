d70e8f9a323353fae52f98afb50f281296f63436
Maloka-119 <aishafathy647@gmail.com>
2025-11-18 19:42:48 +0200

    feedback in backend

---
+++ Backend-Alumni/src/routes/feedback.route.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+const feedbackController = require("../controllers/feedback.controller");
+const protect = require("../middleware/authMiddleware");
+
+router.post("/", protect, feedbackController.createFeedback);
+router.get("/", protect,feedbackController. getAllFeedback);
+router.get("/graduate/:id", protect, feedbackController.getGraduateFeedback);
+router.get("/category/:category", protect, feedbackController.getByCategory);
+router.put("/:id", protect, feedbackController.updateFeedback);
+router.delete("/:id", protect, feedbackController.deleteFeedback);
+
+module.exports = router;
