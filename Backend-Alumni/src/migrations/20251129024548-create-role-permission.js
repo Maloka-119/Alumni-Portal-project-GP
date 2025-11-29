8adc224924f6b76e33e26408193a941d8159e158
Maloka-119 <aishafathy647@gmail.com>
2025-11-29 05:21:26 +0200

    fix time

---
+++ Backend-Alumni/src/migrations/20251129024548-create-role-permission.js
@@ -0,0 +1,59 @@
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('RolePermission', {
+      role_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Role',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+      },
+      permission_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Permission',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+      },
+      'can-view': {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false
+      },
+      'can-edit': {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false
+      },
+      'can-delete': {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false
+      },
+      'can-add': {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false
+      },
+      'created-at': {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW
+      },
+      'updated-at': {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW
+      }
+    });
+
+    await queryInterface.addConstraint('RolePermission', {
+      fields: ['role_id', 'permission_id'],
+      type: 'primary key'
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('RolePermission');
+  }
+};
