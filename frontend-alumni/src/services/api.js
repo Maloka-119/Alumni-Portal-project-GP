3890304d6ef730b752d706a0bd921e3328407ef9
Maloka-119 <aishafathy647@gmail.com>
2025-12-01 21:39:59 +0200

    dynamic lan

--- frontend-alumni/src/services/api.js
+++ frontend-alumni/src/services/api.js
@@ -1,17 +1,21 @@
 import axios from "axios";
+import i18n from "../i18n/i18n";
 
 const API = axios.create({
   baseURL: "http://localhost:5005/alumni-portal",
-  withCredentials: true, // Enable cookies for session management
+  withCredentials: true,
 });
 
-// Interceptor to attach token automatically
 API.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem("token");
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
+
+    // هنا بنضيف اللغة الحالية للـ backend
+    config.headers["Accept-Language"] = i18n.language;
+
     return config;
   },
   (error) => Promise.reject(error)
